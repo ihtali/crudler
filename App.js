948e9f2deb3c17db86698cd53c70bca399c6470b
ihtali <ihtashamia786@icloud.com>
2024-10-03 20:17:03 +0100

    update app.js

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+///////////////
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ModuleListScreen from './src/component/screens/ModuleListScreen';
+import ModuleAddScreen from './src/component/screens/ModuleAddScreen';
+import ModuleModifyScreen from './src/component/screens/ModuleModifyScreen';
+import ModuleViewScreen from './src/component/screens/ModuleViewScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName='ModuleListScreen'
+        screenOptions={{
+          headerStyle: { backgroundColor: 'black' },
+          headerTintColor: 'white',
+        }}
+      >
+        <Stack.Screen
+          name='ModuleListScreen'
+          component={ModuleListScreen}
+          options={{ title: 'List modules' }}
+        />
+        <Stack.Screen
+          name='ModuleAddScreen'
+          component={ModuleAddScreen}
+          options={{ title: 'Add modules' }}
+        />
+        <Stack.Screen
+          name='ModuleViewScreen'
+          component={ModuleViewScreen}
+          options={{ title: 'View modules' }}
+        />
+        <Stack.Screen
+          name='ModuleModifyScreen'
+          component={ModuleModifyScreen}
+          options={{ title: 'Modify modules' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
