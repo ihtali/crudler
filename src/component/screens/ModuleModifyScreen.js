5e3f6782ec09ff50c18cd69a6655b0cea6e3a0a1
ihtali <ihtashamia786@icloud.com>
2024-10-03 20:11:21 +0100

    Completed 2nd vedio changes
    nm

---
+++ src/component/screens/ModuleModifyScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Screen from '../layout/Screen';
+
+const ModuleModifyScreen = () => {
+  return (
+    <Screen>
+      <Text>Modify</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleModifyScreen;
+ 
---
+++ src/component/screens/ModuleViewScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Screen from '../layout/Screen';
+
+const ModuleViewScreen = () => {
+  return (
+    <Screen>
+      <Text>Viewww</Text>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleViewScreen;
+ 
