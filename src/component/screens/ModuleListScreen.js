26dd2fd57b3b0293ec2eefd6c087b50c28ed08f8
ihtali <ihtashamia786@icloud.com>
2024-10-04 10:10:37 +0100

    Including CRUDL state

--- src/component/screens/ModuleListScreen.js
+++ src/component/screens/ModuleListScreen.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import Screen from '../layout/Screen';
 import initialModules from '../data/modules.js'
@@ -5,13 +6,20 @@ import initialModules from '../data/modules.js'
 import ModuleList from '../entity/modules/ModuleList.js';
 
 const ModuleListScreen = () => {
-  const modules =initialModules;
 
 
-const handleSelect = (module) => alert(`item ${module.ModuleCode} selected`);
+ const [modules , setModules]= useState(initialModules);
+
+
+
+const handleDelete = (module) => 
+   setModules(modules.filter((item) =>  item.ModuleID !==module.ModuleID));
+
+
+
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect}/>
+      <ModuleList modules={modules} onSelect={handleDelete}/>
     </Screen>
   );
 };
