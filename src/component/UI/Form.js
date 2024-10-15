32c85397d3425dc619477dbb958a42ab5eadc507
ihtali <ihtashamia786@icloud.com>
2024-10-15 19:08:45 +0100

    mm

---
+++ src/component/UI/Form.js
@@ -0,0 +1,137 @@
+import { KeyboardAvoidingView,ScrollView,TextInput, StyleSheet, Text, View } from 'react-native';
+///import { Picker } from '@react-native-picker/picker';
+import RNPickerSelect from "react-native-picker-select";
+
+
+import {Button,ButtonTray} from './Button.js';
+import Icons from './Icons.js';
+
+
+const Form = ({children,onSubmit,onCancel ,submitLabel,submitIcon}) => {
+// Initialisations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+return (
+    <KeyboardAvoidingView style={styles.formContainer}>
+    <ScrollView contentContainerStyle={styles.formItems}>  
+     {children}
+     </ScrollView>   
+     
+     <ButtonTray>
+      <Button label={submitLabel} icon={submitIcon} onClick={onSubmit}/>
+      <Button label="Cancel" icon={<Icons.Cancel />}onClick={onCancel}/>
+
+      </ButtonTray>
+
+    </KeyboardAvoidingView>
+);
+};
+
+
+const InputText = ({label,value,onChange})  => {
+    // Initialisations ---------------------
+    // State -------------------------------
+    // Handlers ----------------------------
+    // View --------------------------------
+    return (
+        <View style={styles.item} >
+      <Text style={styles.itemLabel}>{label}</Text>
+      <TextInput 
+      value={value} 
+      onChangeText={onChange} 
+      style={styles.itemTextInput}/>
+     </View>
+    );
+    };
+
+    const InputSelect = ({label ,prompt,options, value,onChange})  => {
+        // Initialisations ---------------------
+        // State -------------------------------
+        // Handlers ----------------------------
+        // View --------------------------------
+        return (
+       <View style={styles.item}> 
+         <Text style={styles.itemLabel}>{label}</Text>
+      
+      <RNPickerSelect
+        mode="dropdown"
+        selectedValue={value}
+        onValueChange={onChange}
+        placeholder={{ label: prompt, value: null, color: "whitesmoke" }}
+        items={options.map((option) => ({
+          label: option.label,
+          value: option.value,
+        }))}
+        style={{
+          inputIOS: styles.inputIOS,
+          inputAndroid: styles.inputAndroid,
+          placeholder: styles.placeholder,
+        }}
+        ></RNPickerSelect>
+    </View>
+  );
+  };
+         
+       
+
+// Compose Component
+Form.InputText= InputText;
+Form.InputSelect= InputSelect;
+
+
+// Styles
+
+const styles = StyleSheet.create({
+    inputIOS: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        borderRadius: 5,
+        paddingLeft: 10,
+        marginBottom: 10,
+      },
+      inputAndroid: {
+        height: 50,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        borderRadius: 5,
+        paddingLeft: 10,
+        marginBottom: 10,
+      },
+      placeholder: {
+        color: "gray",
+      },
+    formContainer:{
+        gap:10,
+    },
+    formItems:{
+        gap:5,
+    },
+
+    itemLabel:{
+        color:'grey',
+        fontSize:16,
+        marginBottom:5,
+      },
+      itemTextInput:{
+        height:50,
+        paddingLeft:10,
+        fontSize:16,
+        backgroundColor:'white',
+        borderRadius:7,
+        borderWidth:1,
+        borderColor:'lightgrey',
+    
+      },
+      itemPickerStyle: {
+        height: 50,
+        backgroundColor: 'whitesmoke',
+      },
+      itemPickerPromptStyle:{
+       color:'gray',    
+  },
+});
+
+
+export default Form;
---
+++ src/component/entity/modules/ModuleForm.js
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { TextInput, StyleSheet, Text, View } from 'react-native';
+import Icons from '../../UI/Icons.js';
+import Form from '../../UI/Form.js';
+
+const defaultModule = {
+    ModuleID: null,
+    ModuleName: null,
+    ModuleCode: null,
+    ModuleLevel: null,
+    ModuleLeaderID: null,
+    ModuleLeaderName: null,
+    ModuleImage: null,
+};
+
+const ModuleForm = ({ onSubmit, onCancel }) => {
+    // Initialisations ---------------------
+    defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
+    defaultModule.ModuleImage = 'https://images.freeimages.com/images/small-previews/9b8/electronic-components-2-1242738.jpg';
+
+    const levels = [
+        { value: 3, label: '3 (Foundation)' },
+        { value: 4, label: '4 (First Year)' },
+        { value: 5, label: '5 (Second Year)' },
+        { value: 6, label: '6 (Final Year)' },
+        { value: 7, label: '7 (Masters)' },
+    ];
+
+    // State -------------------------------
+    const [module, setModule] = useState(defaultModule);
+
+    // Handlers ----------------------------
+    const handleChange = (field, value) => setModule({ ...module, [field]: value });
+    const handleSubmit = () => onSubmit(module);
+
+    // View --------------------------------
+    const submitLabel = 'Add';
+    const submitIcon = <Icons.Add />;
+
+    return (
+        <Form
+            onSubmit={handleSubmit}
+            onCancel={onCancel}
+            submitIcon={submitIcon}
+            submitLabel={submitLabel}
+        >
+            <Form.InputText
+                label="Module Code"
+                value={module.ModuleCode}
+                onChange={(value) => handleChange('ModuleCode', value)}
+            />
+
+            <Form.InputText
+                label="Module Name"
+                value={module.ModuleName}
+                onChange={(value) => handleChange('ModuleName', value)}
+            />
+
+            <Form.InputSelect
+                label="Module Levlel"
+                prompt="Select module level..."
+                options={levels}
+                value={module.ModuleLevel}
+                onChange={(value) => handleChange('ModuleLevel', value)}
+            />
+
+            <Form.InputText
+                label="Module Leader"
+                value={module.ModuleLeaderName}
+                onChange={(value) => handleChange('ModuleLeaderName', value)}
+            />
+
+            <Form.InputText
+                label="Module Image URL"
+                value={module.ModuleImage}
+                onChange={(value) => handleChange('ModuleImage', value)}
+            />
+        </Form>
+    );
+};
+
+const styles = StyleSheet.create({
+    itemLabel: {
+        color: 'grey',
+        fontSize: 16,
+        marginBottom: 5,
+    },
+    itemTextInput: {
+        height: 50,
+        paddingLeft: 10,
+        fontSize: 16,
+        backgroundColor: 'white',
+        borderRadius: 7,
+        borderWidth: 1,
+        borderColor: 'lightgrey',
+    },
+});
+
+export default ModuleForm;
