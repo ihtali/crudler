cc04464b447230a7b824d5961622311ab3902cd0
ihtali <168653584+ihtali@users.noreply.github.com>
2024-11-07 13:23:14 +0000

    Updated (#12)

--- src/component/screens/ModuleListScreen.js
+++ src/component/screens/ModuleListScreen.js
@@ -1,83 +1,90 @@
-import {ActivityIndicator, Alert, StyleSheet,Text,View } from 'react-native';
-import Screen from '../layout/Screen';
-import Icons from '../UI/Icons.js';
-import {Button,ButtonTray} from '../UI/Button.js';
-import ModuleList from '../entity/modules/ModuleList.js';
-import API from '../API/API.js';
-import useLoad from '../API/useLoad.js';
+import {
+  ActivityIndicator,
+  Alert,
+  LogBox,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Screen from "../layout/Screen";
+import useLoad from "../API/useLoad.js";
+import API from "../API/API.js";
 
-const ModuleListScreen = ({navigation}) => {
+import ModuleList from "../entity/modules/ModuleList.js";
+import Icons from "../UI/Icons.js";
+import { Button, ButtonTray } from "../UI/Button.js";
+export const ModuleListScreen = ({ navigation }) => {
+  //Initializations -------------------------
+  LogBox.ignoreLogs([
+    "Non-serializable values were found in the navigation state",
+  ]);
+  const modulesEndpoint = "https://softwarehub.uk/unibase/api/modules";
 
-  const modulesEndpoint = 'https://softwarehub.uk/unibase/api/modules';
-  ////STate---------
+  //State -----------------------------------
+  const [modules, , isLoading, loadModules] = useLoad(modulesEndpoint);
 
-const [modules, ,isLoading,loadModules] = useLoad(modulesEndpoint);
+  //Handlers --------------------------------
+  const onDelete = async (module) => {
+    const deleteEndpoint = `${modulesEndpoint}/${module.ModueID}`;
+    const result = await API.delete(deleteEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.goBack();
+    } else Alert.alert(result.message);
+  };
 
-//handler
-const onAdd = async (module) => {
-  const result =await API.post(modulesEndpoint,module);
-  if ( result.isSuccess ) {
-   loadModules(modulesEndpoint);
-   navigation.goBack();
-  }
-  else Alert.alert(result.message);
-};
+  const onAdd = async (module) => {
+    const result = await API.post(modulesEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.goBack();
+    } else Alert.alert(result.message);
+  };
+  const onModify = async (module) => {
+    const putEndpoint = `${modulesEndpoint}/${module.ModueID}`;
+    const result = await API.put(putEndpoint, module);
+    if (result.isSuccess) {
+      loadModules(modulesEndpoint);
+      navigation.navigate("ModuleViewScreen", { module, onDelete, onModify });
+    } else Alert.alert(result.message);
+  };
 
-const onModify = async(module) => {
-  const putEndpoint = `${modulesEndpoint}/${module.ModuleID}`;
-  const result =await API.put(putEndpoint,module);
-  if ( result.isSuccess ) {
-    loadModules(modulesEndpoint);
-    navigation.navigate("ModuleViewScreen",{module,onDelete,onModify});
-   }
-   else Alert.alert(result.message);
- };
+  const goToViewScreen = (module) =>
+    navigation.navigate("ModuleViewScreen", { module, onDelete, onModify });
 
- const onDelete= async(module) =>  {
-  const deleteEndpoint = `${modulesEndpoint}/${module.ModuleID}`;
-  const result =await API.delete(deleteEndpoint,module);
-  if ( result.isSuccess ) {
-   loadModules(modulesEndpoint);
-   navigation.goBack();
-  }
-  else Alert.alert(result.message);
-};
-
-
-const gotoViewSCcreen = (module) => navigation.navigate('ModuleViewScreen',{module,onDelete,onModify});
-
-const gotoAddSCcreen = () => navigation.navigate('ModuleAddScreen',{onAdd});
-///////VIEW
+  const goToAddScreen = () => navigation.navigate("ModuleAddScreen", { onAdd });
+  //View ------------------------------------
   return (
     <Screen>
- Activity-15--API-Post,-Put-and-Delete
       <ButtonTray>
-      <Button label="Add Modules" icon={<Icons.Add />}onClick={gotoAddSCcreen}/>
+        <Button
+          icon={<Icons.Add />}
+          label="Add"
+          styleButton={{ backgroundColor: "grey" }}
+          onClick={goToAddScreen}
+        />
       </ButtonTray>
-      { isLoading &&(
-        <View style={styles.spinner}>
-         <Text>Retrieving records from {modulesEndpoint}...</Text>
-      <View style={styles.container}>
-      <ButtonTray>
-      <Button label="Add Modules" icon={<Icons.Add />}onClick={gotoAddSCcreen}/>
-      </ButtonTray>
-      {isLoading &&(
-        <View 
-        style={styles.spinner}>
-         <Text style={styles.loading}>Loading...</Text>
-         <ActivityIndicator size='large' />
+      {isLoading && (
+        <View style={styles.loading}>
+          <Text>Retrieving records from {modulesEndpoint}...</Text>
+          <ActivityIndicator size="large" />
         </View>
-      )      }
-      <ModuleList modules={modules} onSelect={gotoViewSCcreen}/>
-      </View>
-      </Screen> 
-    
+      )}
+      <ModuleList modules={modules} onSelect={goToViewScreen} />
+    </Screen>
   );
 };
 
 const styles = StyleSheet.create({
+  padding: {
+    padding: 25,
+  },
+  loading: {
+    height: 100,
+    gap: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default ModuleListScreen;
- 
-
