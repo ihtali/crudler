a4331fe988aed3694423af20bc7eae39efb53a03
ihtali <168653584+ihtali@users.noreply.github.com>
2024-11-08 14:57:47 +0000

    list of user added (#18)

--- src/component/screens/UserListSreen.js
+++ src/component/screens/UserListSreen.js
@@ -1,15 +1,42 @@
-import { StyleSheet,Text } from "react-native";
+import { StyleSheet,Text,View } from "react-native";
 import Screen from "../layout/Screen";
 
-const UserListScreen =() => {
+import initialUsers from '../data/Users';
 
+const UserListScreen =() => {
+///////////Initiisations
+const users = initialUsers; 
+
+///////////State
+///////////Handlers
+///////////View
     return(
-<Screen>
-<Text>List</Text>
+        <Screen>
+          <View style={styles.container}>
+        {users.map((user)=>{
+        return(
+        <View style={styles.item}>
+           <Text style={styles.Text}>
+         {user.UserFirstname} {user.UserLastname} {user.UserType}
+             </Text>
+         </View>
+        );
+    })}
+    </View>
 </Screen>
     );
 };
 
-const styles =StyleSheet.create();
+const styles =StyleSheet.create({
+    container: {},
+    item: {
+        paddingVertical:24,
+        borderTopWidth: 4,
+        borderColor: 'lightgray',
+    },
+    Text: {
+        fontSize:16,
+    },
+});
 
 export default UserListScreen;
