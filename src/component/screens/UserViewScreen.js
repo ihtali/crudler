3eb61c6fc85c477c34026cc1674417ce32ada12b
ihtali <ihtashamia786@icloud.com>
2024-11-08 14:27:28 +0000

    added

---
+++ src/component/screens/UserViewScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet,Text } from "react-native";
+import Screen from "../layout/Screen";
+
+const UserViewScreen =() => {
+
+    return(
+<Screen>
+    <Text>List</Text>
+</Screen>
+    );
+};
+
+const styles =StyleSheet.create();
+export default UserViewScreen;
