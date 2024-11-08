1a66e995f944462b0717f0090b63462a62135289
ihtali <168653584+ihtali@users.noreply.github.com>
2024-11-08 15:12:29 +0000

    Done (#19)

--- src/component/screens/UserListSreen.js
+++ src/component/screens/UserListSreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet,Text,View } from "react-native";
+import { Pressable, ScrollView,StyleSheet,Text,View } from "react-native";
 import Screen from "../layout/Screen";
 
 import initialUsers from '../data/Users';
@@ -9,20 +9,23 @@ const users = initialUsers;
 
 ///////////State
 ///////////Handlers
+const handleSelect = () => alert("User Selected");
 ///////////View
     return(
         <Screen>
-          <View style={styles.container}>
+        <ScrollView style={styles.container}>
         {users.map((user)=>{
         return(
+         <Pressable onPress={handleSelect}>
         <View style={styles.item}>
            <Text style={styles.Text}>
          {user.UserFirstname} {user.UserLastname} {user.UserType}
              </Text>
          </View>
+         </Pressable>
         );
     })}
-    </View>
+    </ScrollView>
 </Screen>
     );
 };
@@ -31,7 +34,7 @@ const styles =StyleSheet.create({
     container: {},
     item: {
         paddingVertical:24,
-        borderTopWidth: 4,
+        borderTopWidth: 6,
         borderColor: 'lightgray',
     },
     Text: {
